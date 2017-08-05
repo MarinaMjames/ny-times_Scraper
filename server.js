f4ecbcc03c5e077fd9a2c73b04b670f103d5e07f
MarinaMJames <marinajames93@gmail.com>
2017-08-05 16:59:20 -0400

    added jumbotron styling and added models folder and added article model

--- server.js
+++ server.js
@@ -2,7 +2,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-
+// Requiring our Note and Article models
+var Note = require("./models/note.js");
+var Article = require("./models/article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
