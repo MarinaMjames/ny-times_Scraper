4178ed8e0f7ee24c69c47a1dd8671048243fcb60
MarinaMJames <marinajames93@gmail.com>
2017-08-05 15:41:31 -0400

    added public folder and .gitignore to hide node_modules folder

---
+++ server.js
@@ -0,0 +1,102 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
+
+
+// Initialize Express
+var app = express();
+
+// Use body parser with our app
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+// Make public a static dir
+app.use(express.static("public"));
+
+// Database configuration with mongoose
+mongoose.connect("mongodb://heroku_hz6dn1zr:ikbauetepb47p1f2uin2krujkk@ds135963.mlab.com:35963/heroku_hz6dn1zr");
+var db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+
+// Routes
+// ======
+
+// A GET request to scrape the echojs website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with request
+  request("https://www.nytimes.com/", function(error, response, html) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(html);
+    // Now, we grab every h2 within an article tag, and do the following:
+    $("article .story").each(function(i, element) {
+
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this).children("h2").text();
+      result.link = $(this).children("h2").attr("href");
+
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
+    });
+  });
+  // Tell the browser that we finished scraping the text
+  res.send("Scrape Complete");
+});
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function(req, res) {
+
+});
+
+// This will grab an article by it's ObjectId
+app.get("/articles/:id", function(req, res) {
+
+});
+
+// Create a new note or replace an existing note
+app.post("/articles/:id", function(req, res) {
+
+  
+});
+  // TODO
+  // ====
+
+// Listen on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
